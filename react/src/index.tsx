1816e371339f55e3f59ba99b3adc90f9db286b15
wjb <2459666474@qq.com>
2023-03-28 12:22:05 +0800

    fix:新建react文件

--- react/src/index.tsx
+++ react/src/index.tsx
@@ -1,7 +1,8 @@
  
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux';
+import { createStore } from 'redux'
 import App from './App'; 
 
 const root = ReactDOM.createRoot(
