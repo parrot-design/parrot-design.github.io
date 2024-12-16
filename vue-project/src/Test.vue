412a543cbc2371c8c369c2ff014387aa4970ab86
wujiabao <wujiabao@wujiabaodeMacBook-Pro.local>
2024-12-16 18:20:06 +0800

    -a

--- vue-project/src/Test.vue
+++ vue-project/src/Test.vue
@@ -1,24 +1,3 @@
 <template>
-  <Hello :test="1"   class="test" style="color:red"></Hello>
-  <!-- <div :test="1" @click.native="test" class="test" style="color:red"></div> -->
+   <div>Hello World</div>
 </template>
-
-<script> 
-
-export default ({
-  components:{
-    Hello:{
-      template:`<div>{{ test }}</div>`,
-      mounted:function(){
-        console.log("mounted==>",this.__proto__.constructor.options);
-      },
-      props:['test']
-    },
-  },
-  mounted:function(){
-    console.log("mounted==>",this.__proto__.constructor.options);
-  }
-})
-</script>
-
- 
